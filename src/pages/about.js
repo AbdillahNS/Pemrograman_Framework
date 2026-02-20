a3dc37fbe7d8eddf706e5ef5aa08e5be1393838b
AbdillahNS <abdillahnoersaid@gmail.com>
2026-02-20 15:29:11 +0700

    JS 2

---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+const About = () => {
+    return (
+        <div>
+            <p>Nama: Abdillah Noer Said</p>
+            <p>NIM: 2341720018</p>
+            <p>Prodi: Teknik Informatika</p>
+        </div>
+    );
+};
+
+export default About;
