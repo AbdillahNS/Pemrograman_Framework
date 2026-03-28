e9c26c6a8752e9320c1a73d08af8ad7bc6313813
AbdillahNS <abdillahnoersaid@gmail.com>
2026-03-29 00:27:59 +0700

    Jobsheet 8

--- src/pages/produk/[id].tsx
+++ src/pages/produk/[id].tsx
@@ -1,13 +1,22 @@
 import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import useSWR from 'swr';
+import fetcher from '../utils/swr/fetcher';
+import TampilanProduk from '../views/product';
 
 const HalamanProduk = () => {
-    // const Router = useRouter();
-    // console.log(Router);
     const { query } = useRouter();
+    const { data, error, isLoading } = useSWR('/api/produk', fetcher);
+
     return (
         <div>
-            <h1>Halaman Produk</h1>
-            <p>Produk: {query.id}</p>
+            <div>
+                <h1>Halaman Produk Detail</h1>
+                <p>Produk ID: {query.id}</p>
+            </div>
+            <div>
+                <TampilanProduk products={isLoading ? [] : data?.data || []} />
+            </div>
         </div>
     );
 };
--- src/pages/produk/index.tsx
+++ src/pages/produk/index.tsx
@@ -1,44 +1,17 @@
 import { useRouter } from "next/router";
 import { use, useEffect, useState } from "react";
+import TampilanProduk from "../views/product";
+import useSWR from "swr";
+import fetcher from "../utils/swr/fetcher";
 
-type ProductType = {
-    id: string;
-    name: string;
-    price: number;
-    size: string;
-    category: string;
-}
-
+// const fetcher = (url: string) => fetch(url).then((res) => res.json());
 const kategori = () => {
     const [products, setProducts] = useState([]);
-
-    const fetchProducts = () => {
-        fetch('/api/produk')
-            .then((response) => response.json())
-            .then((responsedata) => {
-                setProducts(responsedata.data);
-            })
-            .catch((error) => {
-                console.error('Error fetching products:', error);
-            });
-    };
-
-    useEffect(() => {
-        fetchProducts();
-    }, []);
+    const { data, error, isLoading } = useSWR("/api/produk", fetcher);
 
     return (
         <div>
-            <h1>Daftar Produk</h1>
-            <button onClick={fetchProducts}>Refresh Produk</button>
-            {products.map((products: ProductType) => (
-                <div key={products.id}>
-                    <h2>{products.name}</h2>
-                    <p>Harga: {products.price}</p>
-                    <p>Ukuran: {products.size}</p>
-                    <p>Kategori: {products.category}</p>
-                </div>
-            ))}
+            <TampilanProduk products={isLoading ? [] : data.data} />
         </div>
     );
 };
