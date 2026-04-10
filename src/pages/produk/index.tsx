15e6fef2e47da320ced4541e851617fcddb3b37e
AbdillahNS <abdillahnoersaid@gmail.com>
2026-04-10 14:38:42 +0700

    Jobsheet 13

--- src/pages/produk/index.tsx
+++ src/pages/produk/index.tsx
@@ -1,9 +1,11 @@
+import { useRouter } from "next/router";
 import { useState } from "react";
 import TampilanProduk from "../../views/product";
 import useSWR from "swr";
 import fetcher from "../../utils/swr/fetcher";
 
 const kategori = () => {
+    const { push } = useRouter();
     const [products, setProducts] = useState([]);
     const { data, error, isLoading } = useSWR("/api/produk", fetcher);
 
